617e6f421913c5ecfdc0ef493103d37e6fb4b1c7
Dominik Ludera <domludera@gmail.com>
2021-07-02 14:54:36 -0400

    backend java setup

---
+++ backend/PrimeHealth/src/test/java/com/ph/PrimHealth/PrimeHealth/PrimeHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ph.PrimHealth.PrimeHealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
