d87ed186ea7abe93a94c7ee32e986def4ffcc923
yacine belaifa SOUNBOUL <yacinebelaifa@live.ca>
2021-07-05 15:54:04 -0300

    added routing

--- frontend/phfrontend/src/App.js
+++ frontend/phfrontend/src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router,Route } from 'react-router-dom';
 import Welcome from './components/Welcome';
 import BookBox from './components/BookBox';
 import AppointmentListBox   from './components/AppointmentListBox';
+import BookAppointment  from './components/BookAppointment';
+
 /*styles*/
 //import 'font-awesome/css/font-awesome.min.css';
 import './App.css';
@@ -20,6 +22,7 @@ function App() {
           <Route path="/" exact component={Welcome}/>
           <Route path="/" exact component={BookBox}/>
           <Route path="/" exact component={AppointmentListBox}/>
+          <Route path="/bookAppointment" exact component={BookAppointment}/>
       </div>
     </Router>
   );
--- frontend/phfrontend/src/components/AppointmentListBox.js
+++ frontend/phfrontend/src/components/AppointmentListBox.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
-
+import { Link } from 'react-router-dom';
 
 class AppointmentListBox extends Component
 {
 
     render()
     {
-        return <div id="app-list-box"><p>List my appointments</p></div>
+        return <div id="app-list-box"><a ><Link to="/AppointmentList" >Appointments List</Link></a></div>
+   
     }
 
 }
