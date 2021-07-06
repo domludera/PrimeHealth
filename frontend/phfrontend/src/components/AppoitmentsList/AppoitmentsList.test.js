b28a1cdaa39426b3ea7e8c7ee3b605f7afc8ebcb
Dominik Ludera <domludera@gmail.com>
2021-07-06 13:22:25 -0400

    clean up

--- frontend/phfrontend/src/components/AppoitmentsList/AppoitmentsList.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppoitmentsList from './AppoitmentsList';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<AppoitmentsList />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
